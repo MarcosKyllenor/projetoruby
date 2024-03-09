1313d97bbd7dcad048709feff89441b2dc44aa78
marcos.junior <marcos.junior@insidesistemas.com.br>
2024-03-08 21:51:31 -0300

    cadastros no banco
    cadastros no banco

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts "Destroying existing records..."
 User.destroy_all
-Debt.destroy_all
 Person.destroy_all
+Debt.destroy_all
 
 User.create email: 'admin@admin.com', password: '111111'
 
@@ -9,28 +9,33 @@ puts "Usuário criado:"
 puts "login admin@admin.com"
 puts "111111"
 
-1000.times do |counter|
-  puts "Creating user #{counter}"
-  User.create email: Faker::Internet.email, password: '111111'
-end
-
-3000.times do |counter|
-  puts "Inserting Person #{counter}"
-
-  attrs = {
-    name: Faker::Name.name,
-    phone_number: Faker::PhoneNumber.phone_number,
-    national_id: CPF.generate,
-    active: [true, false].sample,
-    user: User.order('random()').first
-  }
-  person = Person.create(attrs)
-
-  5.times do |debt_counter|
-    puts "Inserting Debt #{debt_counter}"
-    person.debts.create(
-      amount: Faker::Number.between(from: 1, to: 200),
-      observation: Faker::Lorem.paragraph
+50.times do
+    User.create!(
+      email: Faker::Internet.email,
+      password: Faker::Internet.password(min_length: 6)
     )
   end
-end
+  
+puts "50 usuários foram criados com sucesso!"
+
+100.times do
+    Person.create!(
+        name: Faker::Name.name,
+        phone_number: Faker::PhoneNumber.phone_number,
+        national_id: CPF.generate,
+        active: Faker::Boolean.boolean,
+        user: User.order('random()').first
+      )
+  end
+puts "100 Pessoas foram cadastradas!"
+
+500.times do
+    person = Person.order("RANDOM()").first
+      Debt.create!(
+        person: person,
+        amount: Faker::Number.decimal(l_digits: 2),
+        observation: Faker::Lorem.sentence
+      )
+  end
+
+puts "500 Debitos foram lançados!"
