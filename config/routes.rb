8f87ce37cd9ce93d99f5e92fe4a44a9301768878
marcos.junior <marcos.junior@insidesistemas.com.br>
2024-04-12 20:40:43 -0300

    adicionada pagina de pagamento

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :debts, except: %i(edit update show)
+  resources :payments
 
   resources :people do
     collection do
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,40 +2,47 @@ puts "Destroying existing records..."
 User.destroy_all
 Person.destroy_all
 Debt.destroy_all
+Payment.destroy_all
 
-User.create email: 'admin@admin.com', password: '111111'
+User.create email: 'marcos@admin.com', password: '123456'
 
-puts "Usuário criado:"
-puts "login admin@admin.com"
-puts "111111"
+100.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6)
+  )
+  puts "Usuario cadastrado com sucesso!"
+end
 
-1000.times do
-    User.create!(
-      email: Faker::Internet.email,
-      password: Faker::Internet.password(min_length: 6)
-    )
-  end
-  
-puts "50 usuários foram criados com sucesso!"
 
-3000.times do
-    Person.create!(
-        name: Faker::Name.name,
-        phone_number: Faker::PhoneNumber.phone_number,
-        national_id: CPF.generate,
-        active: Faker::Boolean.boolean,
-        user: User.order('random()').first
-      )
-  end
-puts "100 Pessoas foram cadastradas!"
+100.times do
+  Person.create!(
+    name: Faker::Name.name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    national_id: CPF.generate,
+    active: Faker::Boolean.boolean,
+    user: User.order('random()').first
+  )
+  puts "Cliente cadastrado com sucesso!)"
+end
 
-15000.times do
-    person = Person.order("RANDOM()").first
-      Debt.create!(
-        person: person,
-        amount: Faker::Number.decimal(l_digits: 2),
-        observation: Faker::Lorem.sentence
-      )
-  end
+100.times do
+  person = Person.order("RANDOM()").first
+  Debt.create!(
+    person: person,
+    amount: Faker::Number.decimal(l_digits: 2),
+    observation: Faker::Lorem.sentence
+  )
+  puts "Debito lançado com sucesso!"
+end
 
-puts "500 Debitos foram lançados!"
+100.times do
+  person = Person.order("RANDOM()").first
+  Payment.create!(
+    person: person,
+    amount: Faker::Number.decimal(l_digits: 2),
+    paid_at: Faker::Date.backward(days: 365)
+  )
+  puts "Pagamento realizado com sucesso!"
+end
+
