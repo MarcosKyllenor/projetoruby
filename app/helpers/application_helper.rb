7d6bd459d0eeae4efcf5ad95906f1c3c80f42554
marcos.junior <marcos.junior@insidesistemas.com.br>
2024-04-26 21:49:28 -0300

    Refatoração
    Refatoração

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    def display_notice(notice)
+        if notice.present?
+          content_tag(:div, notice, class: "alert alert-success")
+        end
+    end
+
+    def person_status_icon(person)
+      if person.active
+        content_tag(:span, class: 'text-success') do
+          content_tag(:i, nil, class: 'bi bi-check-circle')
+        end
+      else
+        content_tag(:span, class: 'text-danger') do
+          content_tag(:i, nil, class: 'bi bi-x-circle')
+        end
+      end
+    end
 end
