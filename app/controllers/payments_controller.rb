f25d1f4200d52d61804cdfd36b39bc511f708b83
marcos.junior <marcos.junior@insidesistemas.com.br>
2024-06-27 22:22:02 -0300

    Alterações finais

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,25 +1,17 @@
 class PaymentsController < ApplicationController
   before_action :set_payment, only: %i[ show edit update destroy ]
 
-  # GET /payments or /payments.json
   def index
-    @payments = Payment.all
+    @payments = Payment.paginate(page: params[:page], per_page: 30)
   end
 
-  # GET /payments/1 or /payments/1.json
-  def show
-  end
-
-  # GET /payments/new
   def new
     @payment = Payment.new
   end
 
-  # GET /payments/1/edit
   def edit
   end
 
-  # POST /payments or /payments.json
   def create
     @payment = Payment.new(payment_params)
 
@@ -34,7 +26,6 @@ class PaymentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /payments/1 or /payments/1.json
   def update
     respond_to do |format|
       if @payment.update(payment_params)
@@ -47,7 +38,6 @@ class PaymentsController < ApplicationController
     end
   end
 
-  # DELETE /payments/1 or /payments/1.json
   def destroy
     @payment.destroy!
 
@@ -58,12 +48,10 @@ class PaymentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_payment
       @payment = Payment.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def payment_params
       params.require(:payment).permit(:person_id, :amount, :paid_at)
     end
